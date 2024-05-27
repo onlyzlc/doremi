624395f283a467979f9eea7450d601ef529073cc
zhoulongchun <biubiu0_0@163.com>
2024-05-27 16:21:26 +0800

    记忆音符组

--- doremi/App.js
+++ doremi/App.js
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import IconButton from './components/IconButton'
 import Button from './components/Button'
-import MaterialIcons  from "@expo/vector-icons/MaterialIcons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const roll = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si']
@@ -17,30 +17,12 @@ function Question({ note, showAnswer }) {
   </View>
 }
 
-const storeData = async (value) => {
-  try {
-    const jsonValue = JSON.stringify(value);
-    await AsyncStorage.setItem('lastStatus', jsonValue);
-  } catch (e) {
-    // saving error
-  }
-};
-
-const getData = async () => {
-  try {
-    const jsonValue = await AsyncStorage.getItem('lastStatus');
-    return jsonValue != null ? JSON.parse(jsonValue) : null;
-  } catch (e) {
-    // todo
-  }
-};
-
 export default function App() {
   const totalCount = 10
 
   // 音符组
-  const [noteGroup, setNoteGroup] = useState([1,4,7])
-  const [exerciseLength, setExerciseLength] = useState(totalCount-1)
+  const [noteGroup, setNoteGroup] = useState([])
+  const [exerciseLength, setExerciseLength] = useState(totalCount - 1)
   // 启动状态
   const [practiceStatus, setPracticeStatus] = useState('setting')
   // 回答超时
@@ -50,43 +32,60 @@ export default function App() {
   // 是否禁麦
   const [curfew, setCurfew] = useState(true)
 
-  // 从音符组中随机取一个
-  const getOneNote = () => noteGroup[Math.floor(Math.random() * noteGroup.length)]
   // 随机生成一组不重复的音符
-  const generateGroup = (length=3) => {
-    const arr = [1,2,3,4,5,6,7]
+  const generateGroup = (length = 3) => {
+    const arr = [1, 2, 3, 4, 5, 6, 7]
     let newArr = []
     for (let i = 0; i < length; i++) {
       // 从1~7中随机取出一个加入到新数组中
-      newArr = newArr.concat(arr.splice(Math.floor(Math.random() * (7-i)), 1))
+      newArr = newArr.concat(arr.splice(Math.floor(Math.random() * (7 - i)), 1))
     }
     return newArr
   }
-  const addNote = () => setNoteGroup(generateGroup(noteGroup.length+1))
-  const subtractNote = () => setNoteGroup(generateGroup(noteGroup.length-1))
-  const changeNotes = () => setNoteGroup(generateGroup(noteGroup.length))
-  // 启动训练
-  const practice = () => { 
-    setNote(getOneNote())
-    setExerciseLength(totalCount-1)
-    setPracticeStatus('practicing') 
-    // 存储
-    storeData({noteGroup: noteGroup})
+  // 读取存储
+  if(noteGroup.length === 0){
+    // 教训：更新状态一定要有事件或条件，否则将死循环（因为每次更新状态都会触发所在组件重新执行和渲染）
+    AsyncStorage.getItem('lastStatus')
+      .then((value) => {
+        if(value != null ){
+          // 按上一次练习的音符组数量重新生成一组音符
+          const l = JSON.parse(value).noteGroup.length
+          setNoteGroup(generateGroup(l))
+        }else{
+          setNoteGroup([1,4,7])
+        }
+      })
+      .catch((e) => console.error(e))
   }
   
+  const addNote = () => setNoteGroup(generateGroup(noteGroup.length + 1))
+  const subtractNote = () => setNoteGroup(generateGroup(noteGroup.length - 1))
+  const changeNotes = () => setNoteGroup(generateGroup(noteGroup.length))
+  // 从音符组中随机取一个
+  const getOneNote = () => noteGroup[Math.floor(Math.random() * noteGroup.length)]
+  // 启动训练
+  const practice = () => {
+    setNote(getOneNote())
+    setExerciseLength(totalCount - 1)
+    setPracticeStatus('practicing')
+    // 存储
+    AsyncStorage.setItem('lastStatus', JSON.stringify({ noteGroup: noteGroup }))
+      .catch(e => console.error(e))
+  }
+
   const setNextNumber = () => {
     setIsTimeout(false)
     if (exerciseLength > 0) {
       // 如果没有练完,就计算下一个音符
       setNote(getOneNote())
       setExerciseLength(exerciseLength - 1)
-    } else{
+    } else {
       // 如果练完了,就显示完成图标,短暂停留后自动跳回设置页面
       setPracticeStatus('done')
       setTimeout(() => {
         setPracticeStatus('setting')
       }, 800);
-      
+
       changeNotes()
     }
   }
@@ -107,7 +106,7 @@ export default function App() {
   return (
     <View style={[styles.container, { paddingTop: 64 }]}>
       {/* 未启动 */}
-      {practiceStatus == 'setting' && 
+      {practiceStatus == 'setting' &&
         <View style={styles.content}>
           <View style={styles.titleBar}>
             <Text style={styles.title}>唱名训练</Text>
@@ -119,9 +118,9 @@ export default function App() {
             </View>
             {/* 控制按钮 */}
             <View style={styles.controlBar}>
-              <IconButton onPress={addNote} disabled={noteGroup.length>=7} name="add" label="增加" size={64} />
-              <IconButton onPress={subtractNote} disabled={noteGroup.length<=3} name="remove" label="减少" size={64} />
-              <IconButton onPress={changeNotes} disabled={noteGroup.length==7} name="refresh" label="换一组" size={64} />
+              <IconButton onPress={addNote} disabled={noteGroup.length >= 7} name="add" label="增加" size={64} />
+              <IconButton onPress={subtractNote} disabled={noteGroup.length <= 3} name="remove" label="减少" size={64} />
+              <IconButton onPress={changeNotes} disabled={noteGroup.length == 7} name="refresh" label="换一组" size={64} />
             </View>
           </View>
           {/* 启动按钮 */}
@@ -130,7 +129,7 @@ export default function App() {
           </View>
         </View>}
       {/* 启动后 */}
-      {practiceStatus == 'practicing' && 
+      {practiceStatus == 'practicing' &&
         <View style={styles.content}>
           {/* 顶部栏进度条、关闭按钮 */}
           <View style={styles.titleBar}>
@@ -144,11 +143,11 @@ export default function App() {
           </View>
 
           {/* 语音识别结果展示 */}
-          {curfew ? 
+          {curfew ?
             <View style={styles.heard}>
               <Button label='下一个' onPress={setNextNumber}></Button>
             </View>
-          :
+            :
             <View style={styles.heard}>
               <Text style={{ fontSize: 48, fontWeight: 300, textAlign: 'center' }}>请唱...</Text>
             </View>
@@ -156,9 +155,9 @@ export default function App() {
         </View>}
 
       {/* 完成 */}
-      {practiceStatus == 'done' && 
+      {practiceStatus == 'done' &&
         <View style={styles.content}>
-          <MaterialIcons name='check-circle-outline' size={128} style={{color:'#44D7B6'}} />
+          <MaterialIcons name='check-circle-outline' size={128} style={{ color: '#44D7B6' }} />
         </View>}
       <StatusBar style="auto" hidden={false} />
     </View>
