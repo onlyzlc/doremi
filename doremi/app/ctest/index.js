c233aac260fb2c5de598ef574288633d624c4aca
zhoulongchun <biubiu0_0@163.com>
2024-09-20 17:27:54 +0800

    简易测试页

---
+++ doremi/app/ctest/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DownBeat from "../../components/Downbeat";
+import { View } from "react-native";
+
+export default function Test() {
+  return (
+    <View style={{ padding: 24 }}>
+      <DownBeat />
+    </View>
+  );
+}
